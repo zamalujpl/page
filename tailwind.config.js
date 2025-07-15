fedf9b218b28000ded3a660a9b3b8f85d3db3c88
Mateusz Wieczorek <mateusz.wu@gmail.com>
2025-07-15 22:41:35 +0200

    Switch site to white background and update font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  content: [
-    "./src/**/*.{astro,js,ts,jsx,tsx}",
-    "./public/**/*.html"
-  ],
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx}", "./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "Nunito", "ui-sans-serif", "system-ui", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
+};
